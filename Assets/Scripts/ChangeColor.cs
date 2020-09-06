9c0fcb93c5d7fe0fa6088fcf26f70e721d31705a
jaredrothwell <31265730+jaredrothwell@users.noreply.github.com>
2020-09-06 13:02:51 -0600

    color

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public float timer = 1.0f;
+    public float currentTimer = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTimer += Time.deltaTime;
+        if (currentTimer >= timer)
+        {
+            currentTimer = 0.0f;
+            SpriteRenderer c = GetComponent<SpriteRenderer>();
+            c.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+    }
+}
