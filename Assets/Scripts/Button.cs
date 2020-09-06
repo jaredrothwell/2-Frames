63be68840137b04475b02623f4d7027013989cea
jaredrothwell <31265730+jaredrothwell@users.noreply.github.com>
2020-09-06 12:10:03 -0600

    updated main menu

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    public void loadGame()
+    {
+        Debug.Log("Clicked Play");
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
